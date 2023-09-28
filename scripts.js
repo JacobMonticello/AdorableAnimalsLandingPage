503233842437ba3aeb824966990e0084553f3e10
Jacob monticello <Jacob Monticello>
2023-09-28 13:32:14 -0400

    Updates

--- scripts.js
+++ scripts.js
@@ -17,3 +17,19 @@ function fetchAnimal() {
         })
         .catch(error => console.error('Error fetching adorable animal:', error));
 }
+
+
+function copyCode() {
+    // Get the text field
+    var copyText = document.getElementById("myInput");
+  
+    // Select the text field
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); // For mobile devices
+  
+     // Copy the text inside the text field
+    navigator.clipboard.writeText(copyText.value);
+  
+    // Alert the copied text
+    alert("Copied the text: " + copyText.value);
+}
